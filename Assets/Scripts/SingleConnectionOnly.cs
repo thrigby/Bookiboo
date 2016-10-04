636bbfd70027c511e2f77b40b1699b1189249375
Peter Martin <plutonoodle@gmail.com>
2016-10-04 13:55:18 -0700

    single player machine

---
+++ Assets/Scripts/SingleConnectionOnly.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class SingleConnectionOnly : MonoBehaviour {
+
+	public void BlockOtherConnections ()
+	{
+		GetComponentInParent<NetworkLobbyManager>().maxPlayers = 1;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SinglePlayerMachine.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+//using Prototype.NetworkLobby;
+using UnityEngine.Networking;
+
+
+public class SinglePlayerMachine : NetworkBehaviour {
+
+//	public int resetMaxPlayers;
+
+	void Awake ()
+	{
+		DontDestroyOnLoad (gameObject);
+		Network.maxConnections = 0;
+//		resetMaxPlayers = GetComponent<NetworkLobbyManager>();
+//		resetMaxPlayers. = 1;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
